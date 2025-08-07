4bb5b3f0307227fb7a0e16251980c5f4489bf582
Chris Sirimat <chrissirimat@Chriss-MacBook-Air.local>
2025-08-08 01:03:26 +1000

    added underlines to navbar links when selected

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,36 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import {NavLink } from "react-router-dom";
+
+
 
 export default function Navbar() {
     return (
         <nav className="bg-emerald-200 shadow-md fixed w-full top-0">
             <div className="mx-auto px-15 py-4 flex justify-between items-center">
-                <Link to="/" className="text-xl font-bold text-gray-800 md:text-2xl hover:text-emerald-600 transition-colors duration-200 cursor-pointer">
+                <NavLink to="/" className="text-xl font-bold text-gray-800 hover:text-emerald-600 transition-colors duration-200 cursor-pointer">
                     Chris Sirimat
-                </Link>
+                </NavLink>
                 <div className="flex items-center space-x-8">
-                        <Link to="/" className="nav-link">Home</Link>
-                        <Link to="/about" className="nav-link">About</Link>
-                        <Link to="/projects" className="nav-link">Projects</Link>
-                        <Link to="/contact" className="nav-link">Contact</Link>
+                        <NavLink to="/" className={({isActive}) => 
+                            `nav-link ${isActive ? " underline underline-offset-8" : ""}`
+                        }>
+                            Home
+                        </NavLink>
+                        <NavLink to="/about" className={({isActive}) => 
+                      `nav-link ${isActive ? "underline underline-offset-8" : ""}`
+                        }>
+                            About
+                        </NavLink>
+                        <NavLink to="/projects" className={({isActive}) => 
+                      `nav-link ${isActive ? "underline underline-offset-8" : ""}`
+                        }>
+                            Projects
+                        </NavLink>
+                        <NavLink to="/contact" className={({isActive}) => 
+                      `nav-link ${isActive ? "underline underline-offset-8" : ""}`
+                        }>
+                            Contact
+                        </NavLink>
                 </div>
             </div>
         </nav>
